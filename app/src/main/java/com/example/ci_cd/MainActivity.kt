c2e7a534f9f10edba5c99ef7463934a35e37cd10
AYUSH SRIVASTAVA <ayush7.srivastava@ril.com>
2025-07-14 13:58:56 +0530

    first

--- app/src/main/java/com/example/ci_cd/MainActivity.kt
+++ app/src/main/java/com/example/ci_cd/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.ci_cd
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    fun checkCICD() {
+        Log.d("Test", "successful");
     }
 }
